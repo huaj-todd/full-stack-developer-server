4ecae3cc9bf6cd20eb5a67f2865630b0a92f59e8
huaj-todd <toddhua1994@gmail.com>
2022-03-30 10:03:25 -0400

    Done everything before section 4(Interacting with a Web server using Postman);

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const express = require('express');
+import express from 'express';
+import helloController  from "./controllers/hello-controller.js";
+import userController from "./controllers/user-controller.js";
 const app = express();
-app.get('/hello', (req, res) => {res.send('Hello World!')})
-
+helloController(app);
+userController(app);
+//app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 app.listen(4000);
