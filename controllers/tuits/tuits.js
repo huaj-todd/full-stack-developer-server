a56aec9211858d095408472ea74252e8d717ea5d
huaj-todd <toddhua1994@gmail.com>
2022-04-05 11:15:26 -0400

    Done

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,14 +1,127 @@
 export default [
     {
         "_id": "123",
-        "postedBy": {"username": "NASA"},
-        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
-        "likes": 345
+        "topic": "Web Development",
+        "postedBy": {
+            "username": "ReactJS"
+        },
+        "liked": true,
+        "disliked":false,
+        "verified": false,
+        "handle": "ReactJS",
+        "time": "2h",
+        "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "attachments": {
+            "video": "unKvMC3Y1kI"
+        },
+        "logo-image": "../../../images/react-blue.png",
+        "avatar-image": "../../../images/react-blue.png",
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+
+        },"likes": 45,
+        "dislikes":3244
+    },
+    {
+        "_id": "567",
+        "topic": "Space",
+        "postedBy": {
+            "username": "SpaceX"
+        },
+        "liked": false,
+        "disliked":false,
+        "verified": false,
+        "handle": "spacex",
+        "title": "",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "attachments": {
+            "image": "tuiters/Starship-Mk1-Day.jpeg"
+        },
+        "time": "last month",
+        "logo-image": "../../../images/node.png",
+        "avatar-image": "../../../images/node.png",
+        "tuits": "120K",
+        "likes": 35,
+        "dislikes":23,
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+
+        }
     },
     {
         "_id": "234",
-        "postedBy": { "username": "NASA" },
-        "tuit": "Ingenuity helicopter takes flight.",
-        "likes": 346785
+        "topic": "Space",
+        "postedBy": {
+            "username": "Scott Manley"
+        },
+        "liked": true,
+        "disliked":false,
+        "verified": true,
+        "handle": "DJSnM",
+        "postedOn": "2020-12-10",
+        "time": "1 day",
+        "title": "JavaScript is programming language that can run on browsers as well as desktops",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "logo-image": "../../../images/js.png",
+        "avatar-image": "../../../images/js.png",
+        "tuits": "123K",
+        "dislikes":11,
+        "likes": 4,
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+
+        }
+    },
+    {
+        "_id": "345",
+        "topic": "Web Development",
+        "postedBy": {
+            "username": "jQuery"
+        },
+        "liked": false,
+        "disliked":false,
+        "verified": false,
+        "handle": "jQuery",
+        "title": "jQuery",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "last week",
+        "logo-image": "../../../images/jquery.png",
+        "avatar-image": "../../../images/jquery.png",
+        "tuits": "122K",
+        "dislikes":0,
+        "likes": 1,
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+
+        }
+    },
+    {
+        "_id": "456",
+        "topic": "Web Development",
+        "postedBy": {
+            "username": "NodeJS"
+        },
+        "liked": true,
+        "disliked":false,
+        "verified": true,
+        "handle": "NodeJS",
+        "title": "",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "last month",
+        "logo-image": "../../../images/node.png",
+        "avatar-image": "../../../images/node.png",
+        "tuits": "120K",
+        "dislikes":0,
+        "likes": 1,
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+
+        }
     }
 ];
