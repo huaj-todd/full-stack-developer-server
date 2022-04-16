f7a311bb71dd39ebfb05e166cbd6a7f86c115ffb
huaj-todd <toddhua1994@gmail.com>
2022-04-16 11:40:42 -0400

    Done

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import helloController  from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from 'mongoose';
+const DB_CONNECTION_STRING = "mongodb+srv://todd:<1234567890>@cluster0.im488.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+process.env.DB_CONNECTION_STRING =DB_CONNECTION_STRING
 const CONNECTION_STRING =process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/webdev' ||"mongodb+srv://todd:<1234567890>@cluster0.im488.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const DB_CONNECTION_STRING = "mongodb+srv://todd:<1234567890>@cluster0.im488.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ||'mongodb://localhost:27017/webdev'
-mongoose.connect(DB_CONNECTION_STRING);
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
